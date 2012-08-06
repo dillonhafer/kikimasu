bb3f8501837fb15a2bd58ad0e88c45e5cb662961
Dillon Hafer <dh@dillonhafer.com>
2012-08-05 23:45:33 -0500

    Update allowing manual pause.

--- background.js
+++ background.js
@@ -1,8 +1,12 @@
 // Load url icon
-function onRequest(request, sender, sendResponse) {
+function onRequest(request, sender, sendResponse) {  
   chrome.pageAction.show(sender.tab.id);
   sendResponse({});
 };
+
+chrome.pageAction.onClicked.addListener(function(tab) {
+  chrome.tabs.executeScript(null, {code:"var s =localStorage['pandora_status']; if(s==='enabled'){localStorage['pandora_status']='disabled';document.getElementsByClassName('pauseButton')[0].click();}; if(s==='disabled'){localStorage['pandora_status']='enabled';document.getElementsByClassName('playButton')[0].click();}"});
+});
 
 // Listen for the content script to send a message to the background page.
 chrome.extension.onRequest.addListener(onRequest);
--- contentscript.js
+++ contentscript.js
@@ -1,16 +1,22 @@
+// Initialize local storage variable
+localStorage['pandora_status'] = 'enabled';
+
 function checkIfPlaying() {
   // Check if pandora timed out
   if (document.getElementsByClassName("still_listening")[0] != undefined) {
     document.getElementsByClassName("still_listening")[0].click();
   }
 
-  // Check if pandora is paused
-  if (document.getElementsByClassName("playButton")[0].style['cssText'] == 'display: block; ') {
-    document.getElementsByClassName("playButton")[0].click();
+  // Play if pandora is paused
+  var status = localStorage['pandora_status'];
+  if (status === 'enabled') {
+    if (document.getElementsByClassName("playButton")[0].style['cssText'] == 'display: block; ') {
+      document.getElementsByClassName("playButton")[0].click();
+    }
   }
-  
+
   // Make sure url icon is set
-  chrome.extension.sendRequest({}, function(response) {});  
+  chrome.extension.sendRequest({}, function(response) {});
 }
 
 setInterval(checkIfPlaying, 1000);
