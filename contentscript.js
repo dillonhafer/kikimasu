fce708db934aa2e5051454c631d4a79127525216
Dillon Hafer <dh@dillonhafer.com>
2012-09-02 16:08:25 -0500

    Extension will reload the page after 4 hours. Fixes #1

--- contentscript.js
+++ contentscript.js
@@ -1,7 +1,19 @@
 // Initialize local storage variable
 localStorage['pandora_status'] = 'enabled';
+localStorage['pandora_started_at'] = 0;
 
 function checkIfPlaying() {
+  // Initialize timer
+  var timer = parseInt(localStorage['pandora_started_at']);
+  
+  // Restart after four hours
+  if (timer < 10) {
+    // Start playing after page refresh
+    document.getElementsByClassName("playButton")[0].click();
+  } else if (timer > 14400) {
+    window.location.reload();
+  }
+
   // Check if pandora timed out
   if (document.getElementsByClassName("still_listening")[0] != undefined) {
     document.getElementsByClassName("still_listening")[0].click();
@@ -17,6 +29,9 @@ function checkIfPlaying() {
 
   // Make sure url icon is set
   chrome.extension.sendRequest({}, function(response) {});
+
+  // Increment started at timer  
+  localStorage['pandora_started_at'] = timer + 1;
 }
 
 setInterval(checkIfPlaying, 1000);
