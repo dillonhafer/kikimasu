c6c134fc6cf16d7969503d7beb5abfe772029aec
Dillon Hafer <dh@dillonhafer.com>
2015-06-09 12:33:44 -0400

    Update manifest

--- background.js
+++ background.js
@@ -1,12 +1,24 @@
-// Load url icon
-function onRequest(request, sender, sendResponse) {  
-  chrome.pageAction.show(sender.tab.id);
-  sendResponse({});
-};
+//chrome.pageAction.onClicked.addListener(function(tab) {
+//  chrome.tabs.executeScript(null, {code:"var s =localStorage['pandora_status']; if(s==='enabled'){localStorage['pandora_status']='disabled';document.getElementsByClassName('pauseButton')[0].click();document.getElementsByClassName('playButton')[0].style['cssText']+='background-image: url(/img/player-controls/btn_play.png)'}; if(s==='disabled'){localStorage['pandora_status']='enabled';document.getElementsByClassName('playButton')[0].click();}"});
+//});
 
-chrome.pageAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(null, {code:"var s =localStorage['pandora_status']; if(s==='enabled'){localStorage['pandora_status']='disabled';document.getElementsByClassName('pauseButton')[0].click();document.getElementsByClassName('playButton')[0].style['cssText']+='background-image: url(/img/player-controls/btn_play.png)'}; if(s==='disabled'){localStorage['pandora_status']='enabled';document.getElementsByClassName('playButton')[0].click();}"});
+// When the extension is installed or upgraded ...
+chrome.runtime.onInstalled.addListener(function() {
+  // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // That fires when a page's URL contains a 'g' ...
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: { urlContains: 'www.pandora.com' },
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
+      }
+    ]);
+  });
 });
 
-// Listen for the content script to send a message to the background page.
-chrome.extension.onRequest.addListener(onRequest);
