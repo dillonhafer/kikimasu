1045161e8d2b1d4dfb0091e95313e7f24e445061
Dillon Hafer <dh@dillonhafer.com>
2015-06-09 15:30:44 -0400

    Remove old code

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-//chrome.pageAction.onClicked.addListener(function(tab) {
-//  chrome.tabs.executeScript(null, {code:"var s =localStorage['pandora_status']; if(s==='enabled'){localStorage['pandora_status']='disabled';document.getElementsByClassName('pauseButton')[0].click();document.getElementsByClassName('playButton')[0].style['cssText']+='background-image: url(/img/player-controls/btn_play.png)'}; if(s==='disabled'){localStorage['pandora_status']='enabled';document.getElementsByClassName('playButton')[0].click();}"});
-//});
-
 // When the extension is installed or upgraded ...
 chrome.runtime.onInstalled.addListener(function() {
   // Replace all rules ...
@@ -12,7 +8,7 @@ chrome.runtime.onInstalled.addListener(function() {
         // That fires when a page's URL contains a 'g' ...
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: { urlContains: 'www.pandora.com' },
+            pageUrl: { urlContains: 'pandora.com' },
           })
         ],
         // And shows the extension's page action.
